025173a4172b6f4d1439989b90f22933d6e9db55
Giom <guillaume.duquesnay@gmail.com>
2017-10-27 08:54:34 +0000

    basic single file for unit tests and code

---
+++ requirements.py
@@ -0,0 +1,5 @@
+argparse==1.2.1
+py==1.4.34
+pytest==3.2.2
+wsgiref==0.1.2
+yapf==0.19.0
---
+++ test_coffee_machine.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+# ---- the tests ---- #
+def test_return_something():
+    assert give_coffee_price() is not None
+
+
+def test_return_0_when_it_works():
+    pytest.skip()
+
+
+# ---- the code ---- #
+def give_coffee_price():
+    return None
