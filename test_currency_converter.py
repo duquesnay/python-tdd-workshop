c335b53a6008df2ef66708b23baeef6178b51f0a
duquesnay <guillaume.duquesnay@gmail.com>
2017-11-15 11:22:12 +0000

    add shopping list for the 4 basic currencies

--- test_currency_converter.py
+++ test_currency_converter.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest import skip
 
 
 # ---- the tests ---- #
@@ -7,9 +8,25 @@ def test_return_something():
 
 
 def test_return_SGD_when_SGD():
+    skip()
     assert extract_currency('SGD') == 'SGD'
+
+
+def test_return_EUR_when_EuroSig():
+    skip()
+    assert extract_currency('€') == 'EUR'
+
+
+def test_return_USD_when_Dollar():
+    skip()
+    assert extract_currency('$') == 'USD'
+
+
+def test_return_HKD_when_HkDollar():
+    skip()
+    assert extract_currency('HK$') == 'HKD'
 
 
 # ---- the code ---- #
 def extract_currency(input):
-    return None
+    return ''
