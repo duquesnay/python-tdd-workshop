e67c50373fd4f479f06a128ca31495d044d7dbfd
Guillaume Duquesnay <guillaume.duquesnay@gmail.com>
2017-11-17 01:32:15 +0800

    exercise in almost finished version but cleanable
    - new tests for USD
    - parsing of amount by second function
    - tolerance of space between currency and amount
    - keeping currency as is if no translation provided

--- test_currency_converter.py
+++ test_currency_converter.py
@@ -1,5 +1,5 @@
-import pytest
 from pytest import skip
+import re
 
 
 # ---- the tests ---- #
@@ -8,41 +8,67 @@ def test_return_something():
 
 
 def test_return_SGD_when_SGD():
-    skip()
     assert extract_currency('SGD') == 'SGD'
 
 
 def test_return_EUR_when_EuroSig():
-    skip()
     assert extract_currency('€') == 'EUR'
 
 
 def test_return_USD_when_Dollar():
-    skip()
     assert extract_currency('$') == 'USD'
 
 
 def test_return_HKD_when_HkDollar():
-    skip()
     assert extract_currency('HK$') == 'HKD'
 
-def test_should_extract_SGD_currency_from_number():
-    skip()
-    assert extract_currency('SGD1234.45') == 'SGD'
-
-def test_should_extract_EUR_currency_from_number():
-    skip()
-    assert extract_currency('€1234.45') == 'EUR'
 
 def test_should_extract_SGD_currency_from_number():
-    skip()
     assert extract_currency('SGD1234.45') == 'SGD'
 
+
 def test_should_extract_EUR_currency_from_number():
-    skip()
     assert extract_currency('€1234.45') == 'EUR'
+
+
+def test_should_extract_SGD_currency_with_space_from_number():
+    assert extract_currency('SGD 1234.45') == 'SGD'
+ 
+
+def test_should_extract_EUR_with_space_currency_from_number():
+    assert extract_currency('€ 1234.45') == 'EUR'
+
+
+def test_should_extract_original_if_unknown():
+    assert extract_currency('chibjdi123.456') == 'chibjdi'
+
+
+def test_should_extract_number_with_currency_prefix():
+    assert extract_number('SGD123.45') == '123.45'
+    assert extract_number('SGD2,200.00') == '2,200.00'
+
+
+def test_should_extract_number_with_currency_prefix_and_space():
+    assert extract_number('SGD 123.45') == '123.45'
+    assert extract_number('SGD 2,200.00') == '2,200.00'
+
+
+CURRENCY_DICT={
+            '€': 'EUR',
+            'HK$': 'HKD',
+            '$': 'USD'
+            }
 
 
 # ---- the code ---- #
 def extract_currency(input):
-    return ''
+    prefix = extract_prefix(input)
+    return CURRENCY_DICT.get(prefix, prefix)
+
+
+def extract_prefix(input):
+    return input.rstrip('1234567890,. ')
+    
+    
+def extract_number(input):
+    return re.search(r'(\d|\.|,)+', input).group(0)
